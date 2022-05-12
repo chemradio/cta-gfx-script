63d204bf3b460e9c37303d6424f31bb7d0d0e658
chemradio <chemradio@gmail.com>
2022-05-12 17:43:15 +0600

    Init commit and clear history.

---
+++ Telegram-Automated-Script.jsx
@@ -0,0 +1,20 @@
+﻿fileIncluded = true;
+var currentFile = File($.fileName);
+var gfxScriptFile = File(currentFile.parent.parent.fsName + "/AEScript/CTA-GFX-Automator.jsx").fsName;
+#include 'CTA-GFX-Automator.jsx';
+#include 'parameters.jsx';
+
+uberWindow.close();
+
+try {
+    uberScriptOne(uberParameters);
+    var renderComp = app.project.item(1);
+    var renderQueueItemObject = app.project.renderQueue.items.add(renderComp);
+    renderQueueItemObject.outputModule(1).file = File(uberParameters.outputPath+'/'+uberParameters.descriptor);
+    $.writeln(app.project.renderQueue.canQueueInAME);
+    app.project.renderQueue.queueInAME(true);
+    app.project.close(CloseOptions.DO_NOT_SAVE_CHANGES);
+} catch (errorMessage) {
+    $.writeln('Something went wrong')
+    $.writeln(errorMessage)
+}
---
+++ parameters.jsx
@@ -0,0 +1,32 @@
+uberParameters = {
+        backGroundImage:new File (""),
+
+        foregroundImage:new File (""),
+        isTwoLayer:true,
+
+        audioFile:new File (""),
+        hasAudio:true,
+        
+        animationType:"",
+        backgroundAnimationType:"",
+
+        roundCorners:true,
+        biggerPost:false,
+        staticPost:true,
+        compSlideIn:false,
+        motionBlur:true,
+        
+        quoteEnabled:true,
+        quoteAuthor:"",
+        quoteText:"",
+
+        resolution: [1920, 1080],
+
+        descriptor:"",
+        outputPath:"",
+        
+        fontFamily:"Roboto Condensed",
+        ccVersion:2020,
+        isAutoDoable:true,
+        saveProject:false,
+    }
