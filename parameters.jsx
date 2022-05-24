4332dd965aeb74ab02c1fd7ca5ccdfd29cdc5ac9
chemradio <chemradio@gmail.com>
2022-05-24 17:10:12 +0600

    cleanup parameters.jsx

--- parameters.jsx
+++ parameters.jsx
@@ -1,32 +1,24 @@
 uberParameters = {
-        backGroundImage:new File (""),
-
-        foregroundImage:new File (""),
-        isTwoLayer:true,
-
-        audioFile:new File (""),
-        hasAudio:true,
-        
-        animationType:"",
-        backgroundAnimationType:"",
-
-        roundCorners:true,
-        biggerPost:false,
-        staticPost:true,
-        compSlideIn:false,
-        motionBlur:true,
-        
-        quoteEnabled:true,
-        quoteAuthor:"",
-        quoteText:"",
-
-        resolution: [1920, 1080],
-
-        descriptor:"",
-        outputPath:"",
-        
-        fontFamily:"Roboto Condensed",
-        ccVersion:2020,
-        isAutoDoable:true,
-        saveProject:false,
-    }
+    backGroundImage: '',
+    foregroundImage: '',
+    isTwoLayer: '',
+    audioFile: '',
+    hasAudio: '',
+    animationType: '',
+    backgroundAnimationType: '',
+    roundCorners: '',
+    biggerPost: '',
+    staticPost: '',
+    compSlideIn: '',
+    motionBlur: '',
+    quoteEnabled: '',
+    quoteAuthor: '',
+    quoteText: '',
+    resolution: '',
+    descriptor: '',
+    outputPath: '',
+    fontFamily: '',
+    ccVersion: '',
+    isAutoDoable: '',
+    saveProject: '',
+}
