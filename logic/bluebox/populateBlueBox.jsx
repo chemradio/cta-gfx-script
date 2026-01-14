9ce35f3075629fdbe53ceb260c10e4d1af09ae16
Timur Timaev <chemradio@gmail.com>
2026-01-14 12:22:02 +0600

    bluebox finished

--- logic/bluebox/populateBlueBox.jsx
+++ logic/bluebox/populateBlueBox.jsx
@@ -1,15 +1,16 @@
-function populateBlueBox(nameInput, quoteInput, audioName) {
+function populateBlueBox(nameInput, quoteInput, audioName, blueBoxComp, blueBoxLayer) {
     if (audioName) {
         quoteOutPoint = app.project.item(1).layer(findLayerIdByName(audioName)).outPoint;
     } else {
         quoteOutPoint = app.project.item(1).duration;
     }
 
-    bbLayer = app.project.item(1).layer(findLayerIdByName("Quote-Box"));
-    bbLayer.outPoint = quoteOutPoint;
+    // bbLayer = app.project.item(1).layer(findLayerIdByName("Quote-Box"));
+    // bbLayer.outPoint = quoteOutPoint;
+    blueBoxLayer.outPoint = quoteOutPoint;
 
-    nameLayerText = app.project.item(findAsset("Quote-Box")).layer(2).text.sourceText;
+    nameLayerText = blueBoxComp.nameTextBox.text.sourceText;
     nameLayerText.setValue(nameInput.toUpperCase());
-    quoteLayerText = app.project.item(findAsset("Quote-Box")).layer(3).text.sourceText;
+    quoteLayerText = blueBoxComp.commentTextBox.text.sourceText;
     quoteLayerText.setValue(quoteInput);
 }
